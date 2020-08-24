40da220e8af29c9329542cd887556355dba0e816
Le Binh <lebinh190998@gmail.com>
2020-08-24 12:16:17 +0700

    initial

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API is running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`App started on port ${PORT}`));
