d7653bd46fbe8a6273d7cc7b950d51387ecda46f
Le Binh <lebinh190998@gmail.com>
2020-08-25 00:32:27 +0700

    connect db func & set global config

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+
+    //Exir process
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
