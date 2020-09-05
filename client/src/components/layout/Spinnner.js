aa18f1f60238c6d0187bc5c2f4bdfaa94a4a8f44
Le Binh <lebinh190998@gmail.com>
2020-09-05 11:26:54 +0700

    spinner

---
+++ client/src/components/layout/Spinnner.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import spinner from '../../img/spinner.gif';
+
+const Spinnner = () => {
+  debugger;
+  return (
+    <Fragment>
+      <img
+        src={spinner}
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+        alt='loading...'
+      />
+    </Fragment>
+  );
+};
+
+export default Spinnner;
